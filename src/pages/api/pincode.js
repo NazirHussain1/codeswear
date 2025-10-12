d97c6858c85357bee31faec47c44003619cf0079
NazirHussain1 <119680442+NazirHussain1@users.noreply.github.com>
2025-10-12 13:42:04 +0500

    make a pincode verification

---
+++ src/pages/api/pincode.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+  res.status(200).json([3700,3100,4500,5100,7300]);
+}
--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -1,9 +1,25 @@
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 export default function Post() {
   const router = useRouter();
-  const { slug } = router.query; // dynamic param
-
+  const { slug } = router.query; 
+  const [Service,setService]=useState();
+  const [pin,setPin]=useState();
+  const CheckService =async()=>{
+    let pins= await fetch("http://localhost:3000/api/pincode")
+    let pinJson=await pins.json()
+     console.log(pinJson,pin)
+   if((pinJson.includes(parseInt(pin)))){
+    setService(true)
+  
+   }else{
+    setService(false)
+   }
+  }
+  const onchange=(e)=>{
+    setPin(e.target.value);
+  }
   return (
     <>
       <section className="text-gray-600 body-font overflow-hidden">
@@ -11,8 +27,8 @@ export default function Post() {
     <div className="lg:w-4/5 mx-auto flex flex-wrap">
       <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto px-24 object-cover object-top rounded" src="https://m.media-amazon.com/images/I/81YtgFYwxAL._AC_SX466_.jpg"/>
       <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-        <h2 className="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
-        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
+        <h2 className="text-sm title-font text-gray-500 tracking-widest">CODES WEAR</h2>
+        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">Wear the code</h1>
         <div className="flex mb-4">
           <span className="flex items-center">
             <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-pink-500" viewBox="0 0 24 24">
@@ -77,13 +93,27 @@ export default function Post() {
         </div>
         <div className="flex">
           <span className="title-font font-medium text-2xl text-gray-900">RS.5999.00</span>
-          <button className="flex ml-16 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">AddtoCart</button>
+          <button className="flex ml-6 text-white bg-pink-500 border-0 md:py-2 px-2 md:px-6 focus:outline-none hover:bg-pink-600 rounded">Buy Now</button>
+
+          <button className="flex ml-2 text-white bg-pink-500 border-0 md:py-2 px-2 md:px-6 focus:outline-none hover:bg-pink-600 rounded">AddtoCart</button>
           <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
               <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
             </svg>
           </button>
         </div>
+        <div className='flex mt-3 space-x-2'>
+       <input onChange={onchange} type='text' placeholder='Enter your City Pincode' className='px-3 py-2 rounded border border-gray-200 '/>
+        <button onClick={CheckService} className=' text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded'>Check</button>
+        </div>
+        {(!Service && Service !=null)&&
+          <div className='text-red-700 text-sm mt-3'>
+         Sorry! We can not Deliver at that point
+        </div>}
+        {(Service && Service !=null)&&
+          <div className='text-green-700 text-sm mt-3'>
+         YEH! We can Deliver at that point
+        </div>}
       </div>
     </div>
   </div>
