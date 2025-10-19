77a85190895572fb8e1f01a7bec630a671673c60
NazirHussain1 <119680442+NazirHussain1@users.noreply.github.com>
2025-10-20 00:52:22 +0500

    add login signup page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const login = () => {
@@ -7,6 +8,16 @@ const login = () => {
   <div className="sm:mx-auto sm:w-full sm:max-w-sm">
     <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=pink&shade=600" alt="Your Company" className="mx-auto h-10 w-auto" />
     <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+    <p className="mt-4 text-center text-sm/6 text-gray-500">
+  or
+  <Link
+    href="/signup"
+    className="font-semibold text-pink-600 hover:text-pink-500 mx-2"
+  >
+    Signup
+  </Link>
+</p>
+
   </div>
 
   <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -27,8 +38,14 @@ const login = () => {
           <input id="password" type="password" name="password" required autocomplete="current-password" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-pink-600 sm:text-sm/6" />
         </div>
         <div className="text-sm">
-            <a href="/f" className="font-semibold text-pink-600 hover:text-pink-500">Forgot password?</a>
-          </div>
+  <Link
+    href="/forgot"
+    className="font-semibold text-pink-600 hover:text-pink-500"
+  >
+    Forgot password?
+  </Link>
+</div>
+
       </div>
 
       <div>
@@ -36,10 +53,7 @@ const login = () => {
       </div>
     </form>
 
-    <p className="mt-10 text-center text-sm/6 text-gray-500">
-      Not a member?
-      <a href="#" className="font-semibold text-pink-600 hover:text-pink-500">Start a 14 day free trial</a>
-    </p>
+    
   </div>
 </div> 
     </div> 
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,9 +1,101 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
-const signup= () => {
+const signup = () => {
   return (
-    <div> signup</div>
-  )
-}
+    <div>
+      <div className="flex min-h-full flex-col justify-center px-6 py-10 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=pink&shade=600"
+            alt="Your Company"
+            className="mx-auto h-10 w-auto"
+          />
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+            Sign up to your account
+          </h2>
+          <p className="mt-4 text-center text-sm/6 text-gray-500">
+            or
+            <Link
+              href="/login"
+              className="font-semibold text-pink-600 hover:text-pink-500 mx-2"
+            >
+              login
+            </Link>
+          </p>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <label
+                for="name"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Your Name
+              </label>
+              <div className="mt-1">
+                <input
+                  id="name"
+                  type="name"
+                  name="name"
+                  required
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-pink-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                for="email"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  required
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-pink-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  for="password"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  Password
+                </label>
+              </div>
+              <div className="mt-1">
+                <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  required
+                  autocomplete="current-password"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-pink-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-pink-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-pink-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600"
+              >
+                signup
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default signup;
