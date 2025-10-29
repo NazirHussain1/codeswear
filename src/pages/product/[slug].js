74a42fb2de30cd5ad62ce4c8da6f86e00a80e507
NazirHussain1 <nh534392@gmail.com>
2025-10-29 16:03:35 +0500

    Update [slug].js

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -110,10 +110,12 @@ export default function Post({ addToCart }) {
                   <span className="mr-3">Size</span>
                   <div className="relative">
                     <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-pink-200 focus:border-pink-500 text-base pl-3 pr-10">
-                      <option>SM</option>
+                      <option>S</option>
                       <option>M</option>
                       <option>L</option>
                       <option>XL</option>
+                      <option>XXL</option>
+
                     </select>
                     <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
                       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4" viewBox="0 0 24 24">
@@ -127,6 +129,13 @@ export default function Post({ addToCart }) {
               <div className="flex">
                 <span className="title-font font-medium text-2xl text-gray-900">RS.5999.00</span>
 
+                <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ms-16">
+                  <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
+                  </svg>
+                </button>
+              </div>
+               <div className="flex mt-1 space-x-4 ms-0 ">
                 <button className="flex ml-4 text-white bg-pink-500 border-0 md:py-2 px-2 md:px-6 focus:outline-none hover:bg-pink-600 rounded">
                   Buy Now
                 </button>
@@ -146,13 +155,7 @@ export default function Post({ addToCart }) {
                 >
                   Add to Cart
                 </button>
-
-                <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                  <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
-                  </svg>
-                </button>
-              </div>
+</div>
 
               <div className="flex mt-3 space-x-2">
                 <input
