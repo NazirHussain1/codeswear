a349663614d8d64a8c987182b2d4fca01eb7a302
NazirHussain1 <nh534392@gmail.com>
2025-11-05 23:54:44 +0500

    update the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,16 +17,7 @@ const About = () => {
           coding passion in everyday fashion. Whether you’re working on your next big project 
           or just chilling with your favorite coffee mug, CodeWear has got you covered.
         </p>
-        <p className="text-lg text-gray-700 leading-relaxed">
-          Every product we make reflects the spirit of innovation, dedication, and 
-          community within the tech world. We take pride in our high-quality materials, 
-          unique designs, and a customer-first approach that ensures satisfaction 
-          with every order.
-        </p>
-        <p className="text-lg text-gray-700 leading-relaxed">
-          Thank you for supporting <span className="font-semibold text-gray-900">CodeWear</span> — 
-          more than just apparel, it’s a lifestyle for people who love to code, create, and inspire.
-        </p>
+     
       </div>
     </section>
   );
